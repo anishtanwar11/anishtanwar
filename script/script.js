9c913c695a123bb55ebf0ac4c9378b25c5ac27e9
Anish <anishtanvar@gmail.com>
2023-09-20 20:08:06 +0530

    Add skills section and update nave-options

--- script/script.js
+++ script/script.js
@@ -28,3 +28,47 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*==================== SKILLS TAB ====================*/
+let list = document.querySelectorAll('.tab');
+let itemBox = document.querySelectorAll('.skills__name');
+
+// Function to show a specific data-item
+function showDataItem(dataFilter) {
+    // Hide all data-item elements
+    for (let k = 0; k < itemBox.length; k++) {
+        itemBox[k].classList.remove('active');
+        itemBox[k].classList.add('hide');
+    }
+
+    // Show the data-item that matches the data-filter
+    for (let k = 0; k < itemBox.length; k++) {
+        if (itemBox[k].getAttribute('data-item') == dataFilter) {
+            itemBox[k].classList.remove('hide');
+            itemBox[k].classList.add('active');
+            break;
+        }
+    }
+}
+
+// Add click event listeners to tabs
+for (let i = 0; i < list.length; i++) {
+    list[i].addEventListener('click', function () {
+        for (let j = 0; j < list.length; j++) {
+            list[j].classList.remove('active');
+        }
+        this.classList.add('active');
+        let dataFilter = this.getAttribute('data-filter');
+        showDataItem(dataFilter);
+    });
+}
+
+// Initialize by showing the default data-item
+for (let k = 0; k < itemBox.length; k++) {
+    if (itemBox[k].getAttribute('data-default') == 'true') {
+        showDataItem(itemBox[k].getAttribute('data-item'));
+        break;
+    }
+}
+
